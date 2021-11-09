dd794176175299e11947eaf2bed93ff51f820050
Trover <74741960+Troversones@users.noreply.github.com>
2021-11-09 13:24:02 +0100

    xd

---
+++ Kapcsolat.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Kapcsolat</title>
+</head>
+<body>
+    <div id="container">
+        <ul>
+            <li><a href="hangszerbolt.php">Bemutatkozás</a></li>
+            <li><a href="Forum.php">Fórum</a></li>
+            <li><a class="active" href="Kapcsolat.php">Kapcsolat</a></li>
+            <li><a href="Termekek.php">Termékek</a></li>
+        </ul>
+    </div>
+</body>
+</html>
---
+++ Termekek.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <title>Termékek</title>
+</head>
+<body>
+    <div id="container">
+        <ul>
+            <li><a href="hangszerbolt.php">Bemutatkozás</a></li>
+            <li><a href="#">Fórum</a></li>
+            <li><a href="#">Kapcsolat</a></li>
+            <li><a class="active" href="Termekek.php">Termékek</a></li>
+        </ul>
+    </div>
+</body>
+</html>
